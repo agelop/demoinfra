dc11d017ed395cea808e0e557ed0dce0a6ab7c9a
agelop <aldo.georges@gmail.com>
2018-03-01 11:15:15 -0300

    Add files via upload

---
+++ myservers.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  access_key = "AKIAIFGEQJI7OZYD6CJQ"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
+variable "countapp" {
+  default = 2
+}
+
+variable "countweb" {
+  default = 2
+}
+
+variable "countdb" {
+  default = 1
+}
+
+resource "aws_instance" "appserver" {
+  ami             = "ami-26ebbc5c"
+  instance_type   = "t2.micro"
+  depends_on      = ["aws_instance.dbserver"]
+  key_name        = "awskeys"
+  security_groups = ["default"]
+  count           = "${var.countapp}"
+  tags {
+	Name          = "${format("appserver-%03d", countapp.index + 1)}"
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami             = "ami-26ebbc5c"
+  instance_type   = "t2.micro"
+  depends_on      = ["aws_instance.dbserver"]
+  key_name        = "awskeys"
+  security_groups = ["default"]
+  count           = "${var.countweb}"
+  tags {
+	Name          = "${format("webserver-%03d", countweb.index + 1)}"
+  }
+}
+
+resource "aws_instance" "dbserver" {
+  ami             = "ami-26ebbc5c"
+  instance_type   = "t2.micro"
+  key_name        = "awskeys"
+  security_groups = ["default"]
+  count           = "${var.countdb}"  
+  tags {
+	Name          = "${format("dbserver-%03d", countdb.index + 1)}"
+  }
+}
